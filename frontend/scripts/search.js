9433a2d496e602dc975e7b6147b12962d92a8cff
Mattia <mattia.arrigotti@studenti.unimi.it>
2023-06-05 15:28:37 +0200

    onchange event on text input working and elements are generated into result area

--- frontend/scripts/search.js
+++ frontend/scripts/search.js
@@ -1,9 +1,8 @@
-
-
+let input = document.getElementById("search-bar");
 
 function loadResults() {
 
-    let searchKey = document.getElementById("search-bar").value;
+    let searchKey = input.value;
 
     console.log(searchKey);
 
@@ -15,7 +14,19 @@ function loadResults() {
 
     res.forEach((el) => {
         let toAppend = document.createElement("div");
-        toAppend.innerHTML = el;
+
+        let text = document.createElement("p");
+        text.innerHTML = el;
+
+        let text1 = document.createElement("p");
+        text1.innerHTML = el;
+
+        let text2 = document.createElement("p");
+        text2.innerHTML = el;
+
+        toAppend.append(text);
+        toAppend.append(text1);
+        toAppend.append(text2);
 
         toAppend.classList.add("result");
 
