02e26e4114aec1666d32dfcfef160c05d9e4c0ee
Mattia <mattia.arrigotti@studenti.unimi.it>
2023-06-06 16:12:36 +0200

    profile page started, not very good looking, but nice

--- frontend/routing.js
+++ frontend/routing.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const path = require("path");
 
 app.get("/", (req, res) => {
@@ -8,7 +9,10 @@ app.get("/home", (req, res) => {
     res.sendFile(path.join(__dirname, "/pages/home.html"));
 });
 
-
 app.get("/search", (req, res) => {
     res.sendFile(path.join(__dirname, "/pages/search.html"));
+});
+
+app.get("/profile/:id", (req, res) => {
+    res.sendFile(path.join(__dirname, "/pages/profile.html"));
 });
